2a09d4548542c41c8184459bf36a53cfc982acd6
yilizati abudusaimaiti <yilizatiabudusaimaiti@gmail.com>
2018-07-11 16:02:56 -0700

    a new line of random practice code is added

--- src/test/java/test/Tester.java
+++ src/test/java/test/Tester.java
@@ -1,5 +1,6 @@
 package test;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import base.BrowserDriver;
@@ -9,7 +10,7 @@ public class Tester {
 	public static void main(String[] args) {
 
 		WebDriver driver = new BrowserDriver("CHROME");
-
+		driver.findElement(By.className("random"));
 	}
 
 }
